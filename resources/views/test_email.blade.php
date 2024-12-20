0cabef8b39c692d498594b68be0ee330d8c07dbf
Ionut Cornea <cornea.ionut.ci@gmail.com>
2024-12-21 00:02:52 +0200

    Section 32 - Sending Emails

---
+++ resources/views/test_email.blade.php
@@ -0,0 +1,3 @@
+<h1 style="background: #FF2D20; color: #e5e7eb; padding: 20px">
+    Hello {{$user->name}}
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Mail\UserTestEmail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mail', function () {
+
+    $user = User::find(3);
+    // $users = User::all();
+
+    // foreach ($users as $user)
+    // {
+    //     Mail::to($user)->send(new UserTestEmail($user));
+    // }
+    
+    Mail::to($user)->send(new UserTestEmail($user));
+
+    return 'EMAIL SENT';
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
